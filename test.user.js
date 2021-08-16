bb88698cdc7d9dfc303b91486a046bb69ddf5be1
SANDEEP CHAKRAVARTHI <70456508+somethingsrsocrazy@users.noreply.github.com>
2021-08-16 21:45:26 +0530

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -6,7 +6,7 @@
 // @description  Auto login
 // @match        https://newerp.kluniversity.in/index.php
 // @require      https://unpkg.com/tesseract.js@v2.1.1/dist/tesseract.min.js
-// @require      https://cdn.jsdelivr.net/gh/somethingsrsocrazy/test-login@v1.0.1/test.js
+// @require      https://cdn.jsdelivr.net/gh/somethingsrsocrazy/test-login@v1.0.2/test.js
 // @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
 // @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
 // ==/UserScript==
