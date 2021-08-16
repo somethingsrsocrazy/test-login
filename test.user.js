17cc3879a2346d50dc26f8d96665097b4c75390a
SANDEEP CHAKRAVARTHI <70456508+somethingsrsocrazy@users.noreply.github.com>
2021-08-16 20:50:39 +0530

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -4,8 +4,8 @@
 // @namespace    https://newerp.kluniversity.in/
 // @author       sandeep from kl
 // @description  Auto login
-// @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/master/test.user.js
-// @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/master/test.user.js
+// @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
+// @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
 // @match        https://newerp.kluniversity.in/index.php
 // @require      https://unpkg.com/tesseract.js@v2.1.1/dist/tesseract.min.js
 // @require      https://cdn.jsdelivr.net/gh/somethingsrsocrazy/test-login@v1.0.0/test.js
