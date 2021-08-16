3ceb62df1005a7a0fe5dd65a9d78c1531a77ccc7
SANDEEP CHAKRAVARTHI <70456508+somethingsrsocrazy@users.noreply.github.com>
2021-08-16 21:02:12 +0530

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -4,11 +4,11 @@
 // @namespace    https://newerp.kluniversity.in/
 // @author       sandeep from kl
 // @description  Auto login
-// @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
-// @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
 // @match        https://newerp.kluniversity.in/index.php
 // @require      https://unpkg.com/tesseract.js@v2.1.1/dist/tesseract.min.js
-// @require      https://cdn.jsdelivr.net/gh/somethingsrsocrazy/test-login@v1.0.0/test.js
+// @require      https://cdn.jsdelivr.net/gh/somethingsrsocrazy/test-login@v1.0.1/test.js
+// @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
+// @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
 // ==/UserScript==
 
 // ------------- Change username&password Below -----------
@@ -17,16 +17,3 @@ var username = "**********";
 var password = "**********";
 
 // ------------- Change username&password Above -----------
-
-// ========================================================
-
-// ------------- do not modific the below area ------------
-    var id = document.querySelector("#navbar2 > ul > li:nth-child(3) > a").click()
-    function UandP() {
-        document.getElementById("loginform-username").value = username;
-        document.getElementById("loginform-password").value = password;
-    }
-    setTimeout(function() {
-        UandP();
-    }, 1500);
-// ------------- do not modific the above area ------------
