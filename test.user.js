15c397862718303ad22d92151dfc039659c09927
SANDEEP CHAKRAVARTHI <70456508+somethingsrsocrazy@users.noreply.github.com>
2022-02-08 18:20:57 +0530

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name         Erp auto login
-// @version      1.0.3
+// @version      1.0.4
 // @namespace    https://newerp.kluniversity.in/
-// @author       sandeep from kl
+// @author       sandeep from kl-BCA
 // @description  Auto login
 // @match        https://newerp.kluniversity.in/
 // @match        https://newerp.kluniversity.in/index.php
-// @require      https://unpkg.com/tesseract.js@v2.1.1/dist/tesseract.min.js
+// @require      https://unpkg.com/tesseract.js@2.1.1/dist/tesseract.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/tesseract.js/2.1.1/tesseract.min.js.map
+// @require      https://unpkg.com/tesseract.js@2.1.1/dist/worker.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/tesseract.js/2.1.1/worker.min.js
+// @require      https://unpkg.com/tesseract.js-core@2.2.0/tesseract-core.wasm.js
 // @require      https://raw.githubusercontent.com/somethingsrsocrazy/test-login/main/test.js
+// @require      http://tessdata.projectnaptha.com/4.0.0_best/eng.traineddata.gz
 // @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
 // @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
 // ==/UserScript==
@@ -22,7 +27,7 @@ var password = "**********";
 // ========================================================
 // ------------- Do not Change the Below ------------------
 
-var loginclick = document.querySelector("#navbar2 > ul > li:nth-child(3) > a").click()
+var loginclick = document.querySelector("#mobile_bg > div:nth-child(1) > div:nth-child(1) > div > div > div > a:nth-child(1)").click()
 setTimeout(function() {UandP();}, 500);
 setTimeout(function() {solver();}, 1000);
 
