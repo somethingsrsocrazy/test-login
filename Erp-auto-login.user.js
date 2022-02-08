7a159edb0bc1fbb8e3dd46c66b7757cbe55c9cf2
SANDEEP CHAKRAVARTHI <70456508+somethingsrsocrazy@users.noreply.github.com>
2022-02-08 18:58:48 +0530

    Update and rename test.user.js to Erp-auto-login.user.js

--- Erp-auto-login.user.js
+++ Erp-auto-login.user.js
@@ -9,9 +9,9 @@
 // @require      https://unpkg.com/tesseract.js@2.1.1/dist/tesseract.min.js
 // @require      https://unpkg.com/tesseract.js@2.1.1/dist/worker.min.js
 // @require      https://unpkg.com/tesseract.js-core@2.2.0/tesseract-core.wasm.js
-// @require      https://raw.githubusercontent.com/somethingsrsocrazy/test-login/main/test.js
-// @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
-// @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/main/test.user.js
+// @require      https://raw.githubusercontent.com/somethingsrsocrazy/test-login/main/Erp-solver.js
+// @updateURL    https://github.com/somethingsrsocrazy/test-login/raw/main/Erp-auto-login.user.js
+// @downloadURL  https://github.com/somethingsrsocrazy/test-login/raw/main/Erp-auto-login.user.js
 // ==/UserScript==
 
 
